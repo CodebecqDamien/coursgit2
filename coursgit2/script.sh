3b3c658c1d04af274f83cdb0aba4cfb2d96fe284
CODEBECQ Damien <codebecqdamienpro@gmail.com>
2025-10-20 15:48:31 +0200

    ajout du script

---
+++ coursgit2/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script : welcome.sh
+# Description : Affiche un message de bienvenue personnalis√©
+
+# Demande le nom de l'utilisateur
+echo "Quel est ton nom ?"
+read name
+
+# Affiche un message de bienvenue
+echo "-----------------------------------"
+echo "Ì±ã Bienvenue, $name ! Ravi de te voir ici."
+echo "-----------------------------------"
